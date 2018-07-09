3f4d2d3e92028fb47ea7b02cd84a3208ee169ccf
LAPTOP-76VB9TC1\Lap UPRR <ighoztmexican@gmail.com>
2018-07-08 21:23:27 -0500

    Initial commit

---
+++ www/js/action.js
@@ -0,0 +1,13 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady(){
+	$('#sig1').click(function(){
+		navigator.notification.beep(1);
+		var nombre_usuario=document.getElementsByName('nombre')[0].value; //$('#nombre').val();
+		localStorage.setItem("nombre", nombre_usuario);
+		navigator.notification.alert("hola " + nombre_usuario, null, "bienvenido", "ok");
+	});
+
+
+
+
+}
